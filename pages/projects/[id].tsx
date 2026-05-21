2be27c2cdd396f83c3a6f64b97572741f2d05dbe
maganadu <sribalas@gmail.com>
2026-05-21 11:00:24 +0530

    Fix: add useExpenses import to project detail

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Layout from '@/components/Layout';
 import Modal from '@/components/Modal';
 import { useInvoices } from '@/context/InvoiceContext';
+import { useExpenses } from '@/context/ExpenseContext';
 import { PROJECTS as SEED_PROJECTS, SHARED_EXPENSES, PO_ITEMS as SEED_PO_ITEMS, DOC_STATUS as SEED_DOC_STATUS } from '@/lib/seedData';
 import CreatableSelect from '@/components/CreatableSelect';
 import Toast from '@/components/Toast';
